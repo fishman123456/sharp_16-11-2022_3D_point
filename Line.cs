ce960dffd4ee61222d7dd73187a124e81aba85b6
F_D <den9152@yandex.ru>
2022-11-16 19:51:06 +0700

    Initial commit

---
+++ Line.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System.Drawing;
+
+namespace sharp_16_11_2022_3D_point
+{
+    internal class P_Line<T> where T : struct
+    {
+        Point<T> one;
+        Point<T> two;
+        public P_Line (Point<T> one,Point <T> two )
+        {
+            this.one = one;
+            this.two = two;
+        }
+        public P_Line (T Ax, T Ay, T Bx, T By) 
+        {
+            one = new Point<T> (Ax, Ay);
+            two = new Point<T> (Bx, By);
+        }
+        public override string ToString()
+        {
+            return $"Прямая от точки {this.one} до точки {this.two}";
+        }
+    }
+}
+
---
+++ Point_3D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_16_11_2022_3D_point
+{
+    internal class Point_3D : Point<int>
+    {
+        public int z1 { get; set; }
+        public Point_3D(int X1, int Y1, int Z1) :base (X1,Y1)
+        {
+
+            this.z1 = Z1;
+        }
+        public override string ToString()
+        {
+            return $"Точка  x {this.x1}, y {this.y1}, z{this.z1}";
+        }
+
+    }
+}
+
